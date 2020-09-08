9df9f137f56ea64fbad0cc6933782cd4689d539e
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-09-08 11:16:18 +0100

    Add tests to verify the corrected issue
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/corretto_works.rb
+++ controls/corretto_works.rb
@@ -1,25 +1,22 @@
-title 'Tests to confirm corretto8 works as expected'
+title 'Tests to confirm corretto works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
-plan_name = input('plan_name', value: 'corretto8')
+plan_name = input('plan_name', value: 'corretto')
 
-control 'core-plans-corretto8-works' do
+control 'core-plans-corretto-works' do
   impact 1.0
-  title 'Ensure corretto8 works as expected'
+  title 'Ensure corretto works as expected'
   desc '
-  Verify corretto8 by ensuring that
+  Verify corretto by ensuring that
   (1) its installation directory exists 
-  (2) all binaries, with exception of jconsole, orbd, and
-      java-rmi.cgi, return expected output. The exceptions are ignored here 
-      since they are not easily tested: for example jconsole needs a jvm to connect to; orbd
-      starts a daemon, etc
+  (2) all binaries, with exception of jconsole, return 
+      expected output: jconsole needs a jvm to connect to.
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
   end
   
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
@@ -29,99 +26,88 @@ control 'core-plans-corretto8-works' do
     },
     "jar" => {
       command_suffix: "",
-      io: "stderr",
       exit_pattern: /^[^0]{1}\d*$/,
     },
     "jarsigner" => {},
-    "java" => {
-      io: "stderr",
-    },
-    "javac" => {
-    },
-    "javadoc" => {},
-    "javap" => {},
-    "jcmd" => {
+    "java" => {},
+    "javac" => {},
+    "javadoc" => {
+      command_suffix: "--",
       exit_pattern: /^[^0]{1}\d*$/,
     },
+    "javap" => {},
+    "jcmd" => {},
     # ignore because we need a JVM to connect to...
     # "jconsole" => {},
-    "jdb" => {
+    "jdb" => {},
+    "jdeprscan" => {
       exit_pattern: /^[^0]{1}\d*$/,
     },
-    "jdeprscan" => {},
     "jdeps" => {},
-    "jfr" => {},
-    "jhsdb" => {},
-    "jimage" => {},
+    "jfr" => {
+      command_output_pattern: /Tool for working with Flight Recorder files/,
+      command_suffix: "help",
+    },
+    "jhsdb" => {
+      command_suffix: "--help",
+      command_output_pattern: /clhsdb\s+command line debugger/,
+    },
+    "jimage" => {
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
     "jinfo" => {
-      io: "stderr",
-      command_output_pattern: /jinfo \[option\] <pid>/,
+      command_suffix: "",
+      exit_pattern: /^[^0]{1}\d*$/,
+      command_output_pattern: /jinfo <option> <pid>/,
     },
     "jjs" => {
-      io: "stderr",
       command_output_pattern: /jjs \[<options>\] <files> \[-- <arguments>\]/,
       exit_pattern: /^[^0]{1}\d*$/,
     },
-    "jlink" => {},
-    "jmap" => {
-      io: "stderr",
-      command_output_pattern: /jmap \[option\] <pid>/,
+    "jlink" => {
+      exit_pattern: /^[^0]{1}\d*$/,
     },
+    "jmap" => {},
     "jmod" => {},
-    "jps" => {
-      io: "stderr",
-    },
-    "jrunscript" => {
-      io: "stderr",
-    },
+    "jps" => {},
+    "jrunscript" => {},
     "jshell" => {},
-    "jstack" => {
-      io: "stderr",
-      command_output_pattern: /jstack \[-l\] <pid>/,
-    },
+    "jstack" => {},
     "jstat" => {},
     "jstatd" => {
-      io: "stderr",
       exit_pattern: /^[^0]{1}\d*$/,
     },
     "keytool" => {
-      io: "stderr",
       command_output_pattern: /Key and Certificate Management Tool/,
     },
-    "pack200" => {
-      exit_pattern: /^[^0]{1}\d*$/,
-    },
+    "pack200" => {},
     "rmic" => {
       exit_pattern: /^[^0]{1}\d*$/,
     },
     "rmid" => {
-      io: "stderr",
+      command_suffix: "timeout 1",
       exit_pattern: /^[^0]{1}\d*$/,
     },
     "rmiregistry" => {
-      io: "stderr",
+      command_suffix: "timeout 1",
       exit_pattern: /^[^0]{1}\d*$/,
     },
     "serialver" => {
-      io: "stderr",
       command_suffix: "",
       exit_pattern: /^[^0]{1}\d*$/,
       command_output_pattern: /use: serialver \[-classpath classpath\]/,
     },
-    "unpack200" => {
-      exit_pattern: /^[^0]{1}\d*$/,
-      io: "stderr",
-    },
+    "unpack200" => {},
   } 
   
   # Use the following to pull out a subset of the above and test progressiveluy
-  subset = fullsuite.select { |key, value| key.to_s.match(/^[j]a.*$/) }
+  subset = fullsuite.select { |key, value| key.to_s.match(/^.*$/) }
 
   # over-ride the defaults below with (command_suffix:, io:, etc)
   subset.each do |binary_name, value|
     # set default values if each binary doesn't define an over-ride
     command_prefix = value[:command_prefix] || ""
-    command_suffix = value[:command_suffix] || "-help"
+    command_suffix = value.has_key?(:command_suffix) ? "#{value[:command_suffix]} 2>&1" : "-help 2>&1"
     command_output_pattern = value[:command_output_pattern] || /usage:(\s+|.*)#{binary_name}/i 
     exit_pattern = value[:exit_pattern] || /^[0]$/ # use /^[^0]{1}\d*$/ for non-zero exit status
     io = value[:io] || "stdout"
@@ -130,7 +116,7 @@ control 'core-plans-corretto8-works' do
     command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", binary_name)
     describe bash("#{command_prefix} #{command_full_path} #{command_suffix}") do
       its('exit_status') { should cmp exit_pattern }
-      its(io) { should match command_output_pattern }
+      its("stdout") { should match command_output_pattern }
     end
   end
 end
